b77951e101ce827ac49f14a21cacd9a6d639c366
Filippo Ferrando Damillano <ferrandodfilippo@protonmail.com>
2024-07-02 10:11:36 +0200

    bettered (maybe) rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,23 @@
 import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
-export const get = () =>
-	rss({
+export const get = async () => {
+	const allItems = import.meta.glob('./blog/**/*.md');
+
+	const items = await Promise.all(Object.keys(allItems).map(async (path) => {
+		const { frontmatter } = await allItems[path]();
+		return {
+			...frontmatter,
+			link: path.replace('./blog', '').replace('.md', ''),
+		};
+	}));
+
+	items.reverse();
+
+	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.md'),
+		items,
 	});
+};
