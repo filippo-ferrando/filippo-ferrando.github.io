d8c259948fe8668892e0e90fadd81158208a95a1
Filippo Ferrando Damillano <ferrandodfilippo@protonmail.com>
2024-07-02 10:17:43 +0200

    nah didnt like it

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,33 +1,10 @@
 import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
-function extractDate(frontmatter) {
-  return new Date(frontmatter.date);
-}
-
-export const get = async () => {
-  const postImports = import.meta.glob('./blog/**/*.md');
-  const postPromises = Object.entries(postImports).map(async ([path, resolver]) => {
-    const { frontmatter } = await resolver();
-    return {
-      ...frontmatter,
-      link: path.replace('./', '/').replace('.md', ''),
-    };
-  });
-
-  const posts = await Promise.all(postPromises);
-
-  const sortedPosts = posts.sort((a, b) => extractDate(b) - extractDate(a));
-
-  return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    site: import.meta.env.SITE,
-    items: sortedPosts.map((post) => ({
-      title: post.title,
-      description: post.description,
-      link: post.link,
-      pubDate: post.date,
-    })),
-  });
-};
+export const get = () =>
+	rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: import.meta.env.SITE,
+		items: import.meta.glob('./blog/**/*.md'),
+	});
